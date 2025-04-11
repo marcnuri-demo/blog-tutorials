68f210d0a4ce9a74a4ad654fc6c93a18f478114c
Marc Nuri <marc@marcnuri.com>
2025-04-11 05:00:25 +0200

    fix: add node env shebang

--- connecting-to-mcp-server-with-langchainjs/index.mjs
+++ connecting-to-mcp-server-with-langchainjs/index.mjs
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import {createReactAgent} from '@langchain/langgraph/prebuilt';
 import {ChatOpenAI} from '@langchain/openai';
 import {loadMcpTools} from '@langchain/mcp-adapters';
